e74f1f663db307a7b7ba23a3486ac691d68841aa
christian <i@hate.coffee>
2023-09-13 23:12:29 -0400

    update

--- build.zig
+++ build.zig
@@ -1,7 +1,21 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("zigwin32", .{
+    _ = b.addModule("win32", .{
         .source_file = .{ .path = "win32.zig" },
     });
+
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    const lib = b.addStaticLibrary(.{
+        // the output will be libduck.a
+        .name = "win32",
+        // the code to our wrapper library
+        .root_source_file = .{ .path = "win32.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(lib);
 }
